fcec531b29d9a27e186f04da0e2ce9717ed7290a
Eliezer Nsengi <nsengitech@gmail.com>
2024-04-13 10:00:49 +0200

    feature: implemented login & logout

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',Home.as_view())
+    path('',Home.as_view()),
+    path('login',Login.as_view()),
+    path('logout',logout)
+
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
-
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 # Create your views here.
 class Home(View):
     def get(self,request):
         return render(request,'index.html')
+
+class Login(View):
+    def get(self,request):
+        return render(request,'login.html')
+    
+    def post(self,request):
+        if request.method == 'POST':
+            username = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/dashboard')
+            else:
+                messages.error(request,'404 ---- User With Given Credentials Was not found!')
+                return render(request, 'login.html')
+
+def Logout(request):
+    logout(request)
+    return redirect('/login')
